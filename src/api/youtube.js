e779f92fa4c459dd9f3327958d3210bd8f545bd9
Hama Amakaye Maman Rabiou <foullo@hotmail.com>
2022-08-30 17:47:14 +0200

    adding the youtube api key with env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
-//const KEY = process.env.YOUTUBE_KEY;
-const KEY = 'AIzaSyBS_Na5mvb6_i7ZPudJAxuNGzhEn0jB_-o';
+const KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
+
+
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY,
+        key: KEY
     }
+
+
 
 });
